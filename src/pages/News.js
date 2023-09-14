9d9b24f220595a33be302ff45f6feef5216e4437
yantangyantang <ytang@unc.edu>
2023-09-14 17:16:52 -0400

    news page drafted

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,17 +1,53 @@
 import * as React from "react"
 import "./styles.css"
 import Header from '../components/header'
+import { Link } from 'gatsby'; 
+import Footer from '../components/Footer';
+
+// Define an array of news items with date information
+const newsItems = [
+  {
+    date: new Date("2023-09-16"),
+    title: "UNC HAMIL has launched the official website on Sep 15th",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus malesuada a libero in condimentum. Nunc suscipit ligula sit amet nibh ultrices, vel condimentum ligula ultrices.",
+  },
+  {
+    date: new Date("2023-09-02"),
+    title: "News item 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus malesuada a libero in condimentum. Nunc suscipit ligula sit amet nibh ultrices, vel condimentum ligula ultrices.",
+  },
+];
+
+// Sort the news items by date (newest first)
+newsItems.sort((a, b) => b.date - a.date);
 
 export default function News() {
   return (
-  <div>
-    <Header />
-    <h1>News</h1>
-    <ul>
-        <li>News item 1</li>
-        <li>News item 2</li>
-        {/* Add news updates about your lab */}
-    </ul>
-  </div>
-  )
+    <div>
+      <Header />
+      <div className="news-container">
+        <h1>News</h1>
+        <ul className="news-list">
+          {newsItems.map((news, index) => (
+            <li className="news-item" key={index}>
+              <span className="news-date">
+                {news.date.toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </span>
+              <h2>
+                <a href={`/news/news${index + 1}/`}>{news.title}</a>
+              </h2>
+              <p>{news.description}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Footer />
+    </div>
+  );
 }
---
+++ src/pages/news/news1.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import "../styles.css"
+import Header from '../../components/header';
+import Footer from '../../components/Footer';
+
+export default function news1() {
+  return (
+  <div>
+    <Header />
+    <h1>UNC HAMIL has launched official website on Sep 15th</h1>
+    <p>UNC HAMIL has launched official website on Sep 15th, 2023. The event happend .. UNC HAMIL has launched official website on Sep 15th, 2023. The event happend ..</p>
+    <Footer />
+  </div>
+  )
+}
